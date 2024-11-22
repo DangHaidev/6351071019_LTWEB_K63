d25d33ad18f9fe74fae3cf139dbffdef0e1f8ca2
Hai Dang <dangd408@gmail.com>
2024-11-22 16:12:32 +0700

    first

---
+++ WebLab/Controllers/QuanLyXeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLab.Models;
+
+namespace WebLab.Controllers
+{
+    public class QuanLyXeController : Controller
+    {
+        QLBanXeGanMayEntities db = new QLBanXeGanMayEntities();
+        // GET: QuanLyXe
+        public ActionResult Index()
+        {
+            var data = db.XEGANMAY.ToList()
+                .OrderByDescending(p => p.Ngaycapnhat)
+                .Take(5);
+            return View(data);
+        }
+
+        public ActionResult LoaiXe()
+        {
+            var loaixe = from cd in db.LOAIXE select cd;
+            return View(loaixe);
+        }
+        public ActionResult NhaPhanPhoi()
+        {
+            var nhapp = from cd in db.NHAPHANPHOI select cd;
+            return View(nhapp);
+        }
+        
+        public ActionResult SPTheoLoaiXe(int id)
+        {
+            var nhapp = from cd in db.XEGANMAY where cd.MaLX==id select cd;
+            return View(nhapp);
+        } 
+        public ActionResult SPTheoNhaPP(int id)
+        {
+            var nhapp = from cd in db.XEGANMAY where cd.MaNPP==id select cd;
+            return View(nhapp);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data = db.XEGANMAY.SingleOrDefault(p => p.MaXe == id);
+            return View(data);
+        }
+    }
+}
